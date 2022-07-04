17bf2001031e01b838c36b688b6793e704fce109
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-07-04 15:06:52 -0300

    feat: getBySearch at controller and refactor

--- src/controllers/vehicleController.js
+++ src/controllers/vehicleController.js
@@ -1,53 +1,62 @@
-const vehicleService = require("../services/vehicleService")
+const vehicleService = require('../services/vehicleService');
 
 const create = async (req, res) => {
   const vehicle = req.body;
-  const createVehicle = await vehicleService.create(vehicle);
+  await vehicleService.create(vehicle);
 
-  res.status(201).json(createVehicle);
-}
+  res.status(201).json({ message: 'Veículo cadastrado com sucesso' });
+};
 
 const getAllVehicles = async (_req, res) => {
   const allVehicles = await vehicleService.getAllVehicles();
 
   res.status(200).json(allVehicles);
-}
+};
 
 const getById = async (req, res) => {
   const { id } = req.params;
   const getVehicleById = await vehicleService.getById(id);
 
   res.status(200).json(getVehicleById);
-}
+};
+
+const getBySearch = async (req, res) => {
+  const { q } = req.query;
+  const getQuery = await vehicleService.getBySearch(q);
+  res.status(200).json(getQuery);
+};
 
 const updateDataVehicles = async (req, res) => {
   const { id } = req.params;
-  const { veiculo,marca, ano, descricao, vendido } = req.body;
-  const updateVehicles = await vehicleService.updateDataVehicles(id, veiculo,marca, ano, descricao, vendido);
+  const {
+    veiculo, marca, ano, descricao, vendido,
+  } = req.body;
+  await vehicleService.updateDataVehicles(id, veiculo, marca, ano, descricao, vendido);
 
-  res.status(200).json(updateVehicles);
-}
+  res.status(200).json({ message: 'Veículo atualizado com sucesso' });
+};
 
 const updateSaleVehicle = async (req, res) => {
   const { id } = req.params;
   const { vendido } = req.body;
-  const xablau = await vehicleService.updateSaleVehicle(vendido, id);
+  await vehicleService.updateSaleVehicle(vendido, id);
 
-  res.status(200).json(xablau)
-}
+  res.status(200).json({ message: 'Produto vendido' });
+};
 
 const removeVehicle = async (req, res) => {
   const { id } = req.params;
   await vehicleService.removeVehicle(id);
 
-  res.status(200).end();
-}
+  res.status(204).end();
+};
 
 module.exports = {
   create,
   getAllVehicles,
   getById,
+  getBySearch,
   updateDataVehicles,
   removeVehicle,
   updateSaleVehicle,
-}
+};
