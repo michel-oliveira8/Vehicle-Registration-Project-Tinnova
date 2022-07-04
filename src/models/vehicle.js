9357ab9311abdd656b546bb86e3501b12fe462f2
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-07-04 15:09:49 -0300

    refactor: lint

--- src/models/vehicle.js
+++ src/models/vehicle.js
@@ -1,16 +1,17 @@
-const Vehicle = (sequelize, DataTypes) => {
-  const Vehicle = sequelize.define("Vehicle", {
-    veiculo: DataTypes.STRING,
-    marca: DataTypes.STRING,
-    ano: DataTypes.INTEGER,
-    descricao: DataTypes.STRING,
-    vendido: DataTypes.BOOLEAN,
-  },
-  {
-    timestamps: true,
-  });
+module.exports = (sequelize, DataTypes) => {
+  const Vehicle = sequelize.define(
+    'Vehicle',
+    {
+      veiculo: DataTypes.STRING,
+      marca: DataTypes.STRING,
+      ano: DataTypes.INTEGER,
+      descricao: DataTypes.STRING,
+      vendido: DataTypes.BOOLEAN,
+    },
+    {
+      timestamps: true,
+    },
+  );
 
   return Vehicle;
 };
-
-module.exports = Vehicle;
