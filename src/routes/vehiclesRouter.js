ace3aa5aa530da8f70d33cfad34eda13b7691ba9
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-07-04 17:27:09 -0300

    feat: add mais rotas

--- src/routes/vehiclesRouter.js
+++ src/routes/vehiclesRouter.js
@@ -5,7 +5,11 @@ const vehicleRouter = express.Router();
 
 vehicleRouter.post('/veiculos', vehicleController.create);
 vehicleRouter.get('/veiculos', vehicleController.getAllVehicles);
-vehicleRouter.get('/veiculos/find/search', vehicleController.getBySearch);
+
+vehicleRouter.get('/veiculos/find/search', vehicleController.getBySearchMarca);
+vehicleRouter.get('/veiculos/find/year', vehicleController.getBySearchAno);
+vehicleRouter.get('/veiculos/find/sale', vehicleController.getBySearchVendido);
+
 vehicleRouter.get('/veiculos/:id', vehicleController.getById);
 vehicleRouter.put('/veiculos/:id', vehicleController.updateDataVehicles);
 vehicleRouter.patch('/veiculos/:id', vehicleController.updateSaleVehicle);
