bd2e2e8882607f7a1c8b50961d2e89da590f079e
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-07-04 15:08:31 -0300

    refactor: lint migrations

--- src/migrations/20220701193747-create-vehicles.js
+++ src/migrations/20220701193747-create-vehicles.js
@@ -1,39 +1,38 @@
-'use strict';
 module.exports = {
-  up: async(queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Vehicles', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       veiculo: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       marca: {
-        type:Sequelize.STRING
+        type: Sequelize.STRING,
       },
       ano: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       descricao: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       vendido: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-  down: async(queryInterface, _Sequelize) => {
+  down: async (queryInterface) => {
     await queryInterface.dropTable('Vehicles');
-  }
+  },
 };
