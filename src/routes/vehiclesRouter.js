0aa3ac6a14d9c7c96d56abb9e7b37567582b0bc9
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-07-04 15:07:22 -0300

    feat: router veiculos/find/search

--- src/routes/vehiclesRouter.js
+++ src/routes/vehiclesRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const vehicleController = require('../controllers/vehicleController')
+const vehicleController = require('../controllers/vehicleController');
 
-const vehicleRouter = express.Router()
+const vehicleRouter = express.Router();
 
 vehicleRouter.post('/veiculos', vehicleController.create);
 vehicleRouter.get('/veiculos', vehicleController.getAllVehicles);
+vehicleRouter.get('/veiculos/find/search', vehicleController.getBySearch);
 vehicleRouter.get('/veiculos/:id', vehicleController.getById);
 vehicleRouter.put('/veiculos/:id', vehicleController.updateDataVehicles);
 vehicleRouter.patch('/veiculos/:id', vehicleController.updateSaleVehicle);
