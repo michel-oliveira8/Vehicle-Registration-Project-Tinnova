bb13e976e4221205bd0f738b989c3e46ab9133db
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-07-04 15:09:14 -0300

    feat: function getBySearch at Service and refactor

--- src/services/vehicleService.js
+++ src/services/vehicleService.js
@@ -1,44 +1,59 @@
-const { Vehicle } = require("../models/")
+const { Vehicle } = require('../models');
 
 const create = async (obj) => {
   const createVehicle = await Vehicle.create(obj);
 
   return createVehicle;
-}
+};
+
 const getAllVehicles = async () => {
   const allVehicles = await Vehicle.findAll();
 
   return allVehicles;
-}
+};
 
 const getById = async (id) => {
   const getVehicleById = await Vehicle.findByPk(id);
 
   return getVehicleById;
-}
+};
+
+const getBySearch = async (q) => {
+  const getQuery = await Vehicle.findAll({
+    where: { marca: q },
+  });
+
+  return getQuery;
+};
 
 const updateDataVehicles = async (id, veiculo, marca, ano, descricao, vendido) => {
   await Vehicle.findOne({ where: { id } });
-  await Vehicle.update(
-    {veiculo,marca, ano, descricao, vendido },
-    {where: { id } },
+  const updateAllData = await Vehicle.update(
+    {
+      veiculo, marca, ano, descricao, vendido,
+    },
+    { where: { id } },
   );
-}
+
+  return updateAllData;
+};
 
 const updateSaleVehicle = async (vendido, id) => {
-  return await Vehicle.update({ vendido }, { where: { id }})
-}
+  const vehicleSaled = await Vehicle.update({ vendido }, { where: { id } });
+  return vehicleSaled;
+};
 
 const removeVehicle = async (id) => {
-  const remove = await Vehicle.destroy({where: {id} });
+  const remove = await Vehicle.destroy({ where: { id } });
 
   return remove;
-}
+};
 
 module.exports = {
   create,
   getAllVehicles,
   getById,
+  getBySearch,
   updateDataVehicles,
   removeVehicle,
   updateSaleVehicle,
